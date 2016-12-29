96361b74151400c54c570dc2b8d97d1fa7b2176b
Ed Murphy <emurphy@babycenter.com>
2016-12-29 09:33:48 -0800

    - wrote code to generate all possible expressions using the commutative property. - added test for parenthsized expressions.

--- test/com/lumpofcode/expression/associative/AssociativeTreeHelperTest.java
+++ test/com/lumpofcode/expression/associative/AssociativeTreeHelperTest.java
@@ -64,12 +64,40 @@ public class AssociativeTreeHelperTest
         }
 
         assertEquals("There should be 8 commutations.", 8, commutations.size());
-//        assertTrue("2 * 3 + 4 should be generated", commutations.contains("2 * 3 * 4"));
-//        assertTrue("2 * 4 * 3 should be generated", commutations.contains("2 * 4 * 3"));
-//        assertTrue("3 * 2 * 4 should be generated", commutations.contains("3 * 2 * 4"));
-//        assertTrue("3 * 4 * 2 should be generated", commutations.contains("3 * 4 * 2"));
-//        assertTrue("4 * 2 * 3 should be generated", commutations.contains("4 * 2 * 3"));
-//        assertTrue("4 * 3 * 2 should be generated", commutations.contains("4 * 3 * 2"));
+        assertTrue("2 * 3 + 4 * 5 should be generated", commutations.find("2 * 3 + 4 * 5").isNotEmpty());
+        assertTrue("2 * 3 + 5 * 4 should be generated", commutations.find("2 * 3 + 5 * 4").isNotEmpty());
+        assertTrue("3 * 2 + 4 * 5 should be generated", commutations.find("3 * 2 + 4 * 5").isNotEmpty());
+        assertTrue("3 * 2 + 5 * 4 should be generated", commutations.find("3 * 2 + 5 * 4").isNotEmpty());
+        assertTrue("4 * 5 + 2 * 3 should be generated", commutations.find("4 * 5 + 2 * 3").isNotEmpty());
+        assertTrue("4 * 5 + 3 * 2 should be generated", commutations.find("4 * 5 + 3 * 2").isNotEmpty());
+        assertTrue("5 * 4 + 2 * 3 should be generated", commutations.find("5 * 4 + 2 * 3").isNotEmpty());
+        assertTrue("5 * 4 + 3 * 2 should be generated", commutations.find("5 * 4 + 3 * 2").isNotEmpty());
 
     }
+
+    @Test
+    public void generateCommutedParenthesisExpressionsTest()
+    {
+        LinkList<String> commutations = AssociativeTreeHelper.generateCommutedExpressions("2 * (3 + 4) * 5", new IntegerTruncateFormatter());
+
+        for(LinkList<String> commutation = commutations; commutation.isNotEmpty(); commutation = commutation.tail)
+        {
+            System.out.println(commutation.head);
+        }
+
+        assertEquals("There should be 12 commutations.", 12, commutations.size());
+        assertTrue("2 * (3 + 4) * 5 should be generated", commutations.find("2 * (3 + 4) * 5").isNotEmpty());
+        assertTrue("2 * 5 * (3 + 4) should be generated", commutations.find("2 * 5 * (3 + 4)").isNotEmpty());
+        assertTrue("(3 + 4) * 2 * 5 should be generated", commutations.find("(3 + 4) * 2 * 5").isNotEmpty());
+        assertTrue("(3 + 4) * 5 * 2 should be generated", commutations.find("(3 + 4) * 5 * 2").isNotEmpty());
+        assertTrue("5 * (3 + 4) * 2 should be generated", commutations.find("5 * (3 + 4) * 2").isNotEmpty());
+        assertTrue("5 * 2 * (3 + 4) should be generated", commutations.find("5 * 2 * (3 + 4)").isNotEmpty());
+        assertTrue("2 * (4 + 3) * 5 should be generated", commutations.find("2 * (4 + 3) * 5").isNotEmpty());
+        assertTrue("2 * 5 * (4 + 3) should be generated", commutations.find("2 * 5 * (4 + 3)").isNotEmpty());
+        assertTrue("(4 + 3) * 2 * 5 should be generated", commutations.find("(4 + 3) * 2 * 5").isNotEmpty());
+        assertTrue("(4 + 3) * 5 * 2 should be generated", commutations.find("(4 + 3) * 5 * 2").isNotEmpty());
+        assertTrue("5 * (4 + 3) * 2 should be generated", commutations.find("5 * (4 + 3) * 2").isNotEmpty());
+        assertTrue("5 * 2 * (4 + 3) should be generated", commutations.find("5 * 2 * (4 + 3)").isNotEmpty());
+    }
+
 }
